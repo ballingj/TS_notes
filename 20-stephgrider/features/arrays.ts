59e5c4347a0983cdab87908bbf26538590269440
ballingj <jeffrey.ballinger@gmail.com>
2026-02-15 18:55:39 -0600

    arrays

--- 20-stephgrider/features/arrays.ts
+++ 20-stephgrider/features/arrays.ts
@@ -1,23 +1,29 @@
 const carMakers = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 
-const carsByMake: string[][] = [];
+// Array of string arrays
+let carsByMake: string[][] = [];
+// looks like this
+carsByMake = [
+  ['f150', 'ranger'],
+  ['corolla', 'camry'],
+  ['camaro', 'silverado']
+];
 
-// Help with inference when extracting values
+// Why do we care to use Typed arrays?
+// 1) Help with inference when extracting values
 const car = carMakers[0];
-const myCar = carMakers.pop();
+const myCar = carMakers.pop();  // remove 'chevy'
 
-// Prevent incompatible values
+// 2) Prevent incompatible values
 carMakers.push(100);
 
-// Help with 'map'
-carMakers.map(
-  (car: string): string => {
-    return car.toUpperCase();
-  }
-);
+// 3) Help with 'map', 'forEach', 'reduce'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
 
-// Flexible types
+// 4) Flexible types
 const importantDates: (Date | string)[] = [];
 importantDates.push('2030-10-10');
 importantDates.push(new Date());
--- 20-stephgrider/features/tuples.ts
+++ 20-stephgrider/features/tuples.ts
@@ -4,6 +4,14 @@ const drink = {
   sugar: 40
 };
 
+// this format is an Array
+const coke = ['brown', true, 40];
+coke[0] = 40;  // no errors; we lose the 'brown' value
+
+// we can turn it into a tuple with type annotation
+const coke1: [string, boolean, number] = ['brown', true, 40];
+coke1[0] = 40;  // errors
+
 // Type alias
 type Drink = [string, boolean, number];
 
@@ -11,8 +19,11 @@ const pepsi: Drink = ['brown', true, 40];
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
 
-const carSpecs: [number, number] = [400, 3354];
 
+// case against tuples:  tuples are not intuitive
+const carSpecs: [number, number] = [400, 3354];  // what are these numbers representing?
+
+// use objects instead
 const carStats = {
   horsepower: 400,
   weight: 3354
